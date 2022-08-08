984a0ff559466ec93453e92f67eed7bce45279a7
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-08-08 06:30:25 +0530

    Veux: Bring UP Tree For Veux Voltage OS
    Sidned-off-by: Dragon <dragon1237@gmail.com>
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>
    Signed-off-by: JaswalAshish48 <jaswalashish48@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/voltage_veux.mk
+
+COMMON_LUNCH_CHOICES := \
+    voltage_veux-user \
+    voltage_veux-userdebug \
+    voltage_veux-eng
